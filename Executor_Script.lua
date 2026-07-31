e4ae1e15657349ae6c077cb3578fdfe8793f98b4
adrian <kipperadrian3@gmail.com>
2026-07-31 16:51:19 +0200

    Fix script URL in Executor_Script.lua to point to 99_nights_admin.lua

--- Executor_Script.lua
+++ Executor_Script.lua
@@ -2,4 +2,4 @@
     99 Nights Admin Panel - Executor Loader
     Einfach diesen gesamten Code kopieren und in deinen Executor einfügen!
 ]]
-loadstring(game:HttpGet("https://raw.githubusercontent.com/kipperadrian3-boop/roblox-ui-library/main/main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kipperadrian3-boop/roblox-ui-library/main/99_nights_admin.lua"))()
