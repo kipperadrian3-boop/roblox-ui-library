d92af2411c6d9be3a0c3c81515462804c385b831
adrian <kipperadrian3@gmail.com>
2026-08-01 12:30:34 +0200

    Rename executor script to 99nights_executor_script and add remotespy_executor_script

---
+++ 99nights_executor_script.lua
@@ -0,0 +1,5 @@
+--[[
+    99 Nights Admin Panel - Executor Loader
+    Einfach diesen gesamten Code kopieren und in deinen Executor einfügen!
+]]
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kipperadrian3-boop/roblox-ui-library/main/99_nights_admin.lua"))()
---
+++ remotespy_executor_script.lua
@@ -0,0 +1,5 @@
+--[[
+    Advanced Remote Event Spy - Executor Loader
+    Einfach diesen gesamten Code kopieren und in deinen Executor einfügen!
+]]
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kipperadrian3-boop/roblox-ui-library/main/remote_spy.lua"))()
