143d4e8414a6061f90db31718e6dadc17b6a050d
adrian <kipperadrian3@gmail.com>
2026-08-12 12:01:13 +0200

    Update Grow a Garden loader script with dynamic cache buster

--- growagarden_executor_script.lua
+++ growagarden_executor_script.lua
@@ -2,4 +2,4 @@
     Grow a Garden Suite - Executor Loader
     Einfach diesen gesamten Code kopieren und in deinen Executor einfügen!
 ]]
-loadstring(game:HttpGet("https://raw.githubusercontent.com/kipperadrian3-boop/roblox-ui-library/main/grow_a_garden.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kipperadrian3-boop/roblox-ui-library/main/grow_a_garden.lua?v=" .. tostring(math.random(1, 9999999))))()
