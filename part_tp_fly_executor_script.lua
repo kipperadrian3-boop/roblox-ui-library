c59de7503c2136f03d7cf4ad8fa2292e51a20216
adrian <kipperadrian3@gmail.com>
2026-08-14 17:46:45 +0200

    Update loader script with os.time cache buster to bypass Roblox HttpGet caching

--- part_tp_fly_executor_script.lua
+++ part_tp_fly_executor_script.lua
@@ -2,4 +2,4 @@
     Part Teleport & Fly Suite - Executor Script
     Einfach diesen gesamten Code kopieren und in deinen Executor einfügen!
 ]]
-loadstring(game:HttpGet("https://raw.githubusercontent.com/kipperadrian3-boop/roblox-ui-library/main/part_tp_fly.lua?v=" .. tostring(math.random(1, 9999999))))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kipperadrian3-boop/roblox-ui-library/main/part_tp_fly.lua?v=" .. tostring(os.time()) .. "_" .. tostring(math.random(1, 999999))))()
