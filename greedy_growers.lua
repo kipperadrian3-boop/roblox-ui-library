e07ca65a28af9e0a580e81bc3d21ba0905794250
adrian <kipperadrian3@gmail.com>
2026-09-02 14:13:24 +0200

    Add FindPrompt collection to fruit loop

--- greedy_growers.lua
+++ greedy_growers.lua
@@ -163,7 +163,7 @@ task.spawn(function()
                 for _, desc in ipairs(myPlot:GetDescendants()) do
                     if desc:IsA("ProximityPrompt") then
                         local parent = desc.Parent
-                        if parent and parent.Parent and parent.Parent.Name == "FruitSpawns" then
+                        if desc.Name == "FindPrompt" or (parent and parent.Parent and parent.Parent.Name == "FruitSpawns") then
                             table.insert(promptsToTrigger, desc)
                         end
                     end
